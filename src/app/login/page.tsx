50313ebeca9ec6fbf867259c9bc611c3407bfbd1
adifkhan <pkadif@gmail.com>
2023-07-30 15:34:22 +0600

    userModel defined

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,46 +1,44 @@
-import Link from 'next/link';
-import React from 'react';
-import styles from '@/app/styles/login.module.css';
+'use client';
 
-const page = () => {
+import React, { useState } from 'react';
+import styles from '@/app/styles/login.module.css';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+const Login = () => {
+  const [user, setUser] = useState({});
   return (
-    <div>
-      <section className={styles.login}>
-        <h1 className={styles.heading}>Login</h1>
-        <form>
-          <div className={styles.inputgroup}>
-            <label htmlFor=''>Email</label>
-            <input
-              type='text'
-              placeholder='Enter Email*'
-              className={styles.inputfield}
-            />
-          </div>
-          <div className={styles.inputgroup}>
-            <label htmlFor=''>Password</label>
-            <input
-              type='password'
-              placeholder='Passoerd*'
-              className={styles.inputfield}
-            />
-          </div>
-          <div className={styles.button}>
-            <input
-              type='submit'
-              value='Login'
-              className={styles.submitbutton}
-            />
-          </div>
-        </form>
-        <p>
-          New to Next Auth?{' '}
-          <Link href='/signup' className={styles.link}>
-            Sign Up
-          </Link>
-        </p>
-      </section>
-    </div>
+    <section className={styles.login}>
+      <h1 className={styles.heading}>Login</h1>
+      <form>
+        <div className={styles.inputgroup}>
+          <label htmlFor=''>Email</label>
+          <input
+            type='text'
+            placeholder='Enter Email*'
+            className={styles.inputfield}
+          />
+        </div>
+        <div className={styles.inputgroup}>
+          <label htmlFor=''>Password</label>
+          <input
+            type='password'
+            placeholder='Passoerd*'
+            className={styles.inputfield}
+          />
+        </div>
+        <div className={styles.button}>
+          <input type='submit' value='Login' className={styles.submitbutton} />
+        </div>
+      </form>
+      <p>
+        New to Next Auth?{' '}
+        <Link href='/signup' className={styles.link}>
+          Sign Up
+        </Link>
+      </p>
+    </section>
   );
 };
 
-export default page;
+export default Login;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,66 +1,74 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import styles from '@/app/styles/signup.module.css';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-const page = () => {
+const SignUp = () => {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const onSubmit = async () => {};
   return (
-    <div>
-      <section className={styles.signup}>
-        <h1 className={styles.heading}>Sign Up</h1>
-        <form>
-          <div className={styles.inputgroup}>
-            <label htmlFor=''>Name</label>
-            <input
-              type='text'
-              placeholder='User Name*'
-              className={styles.inputfield}
-            />
-          </div>
-          <div className={styles.inputgroup}>
-            <label htmlFor=''>Email</label>
-            <input
-              type='text'
-              placeholder='Enter Email*'
-              className={styles.inputfield}
-            />
-          </div>
-          <div className={styles.inputgroup}>
-            <label htmlFor=''>Password</label>
-            <input
-              type='password'
-              placeholder='Passoerd*'
-              className={styles.inputfield}
-            />
-          </div>
-          <div className={styles.inputgroup}>
-            <label htmlFor=''>Confirm Password</label>
-            <input
-              type='password'
-              placeholder='Confirm Password*'
-              className={styles.inputfield}
-            />
-          </div>
-          <div>
-            <input type='checkbox' name='terms' id='' />
-            <label htmlFor=''> agree to terms and conditions</label>
-          </div>
-          <div className={styles.button}>
-            <input
-              type='submit'
-              value='Sign Up'
-              className={styles.submitbutton}
-            />
-          </div>
-        </form>
-        <p>
-          Already have an account?{' '}
-          <Link href='/login' className={styles.link}>
-            login
-          </Link>
-        </p>
-      </section>
-    </div>
+    <section className={styles.signup}>
+      <h1 className={styles.heading}>Sign Up</h1>
+      <form>
+        <div className={styles.inputgroup}>
+          <label htmlFor=''>Name</label>
+          <input
+            type='text'
+            placeholder='User Name*'
+            className={styles.inputfield}
+          />
+        </div>
+        <div className={styles.inputgroup}>
+          <label htmlFor=''>Email</label>
+          <input
+            type='text'
+            placeholder='Enter Email*'
+            className={styles.inputfield}
+          />
+        </div>
+        <div className={styles.inputgroup}>
+          <label htmlFor=''>Password</label>
+          <input
+            type='password'
+            placeholder='Passoerd*'
+            className={styles.inputfield}
+          />
+        </div>
+        <div className={styles.inputgroup}>
+          <label htmlFor=''>Confirm Password</label>
+          <input
+            type='password'
+            placeholder='Confirm Password*'
+            className={styles.inputfield}
+          />
+        </div>
+        <div>
+          <input type='checkbox' name='terms' id='' />
+          <label htmlFor=''> agree to terms and conditions</label>
+        </div>
+        <div className={styles.button}>
+          <input
+            type='submit'
+            value='Sign Up'
+            className={styles.submitbutton}
+          />
+        </div>
+      </form>
+      <p>
+        Already have an account?{' '}
+        <Link href='/login' className={styles.link}>
+          Login
+        </Link>
+      </p>
+    </section>
   );
 };
 
-export default page;
+export default SignUp;
